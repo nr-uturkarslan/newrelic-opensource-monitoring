1791325e4e92fa3ef2cbdc87d8ab8d1197c70b31
uturkarslan <ugurturkarslan@gmail.com>
2022-08-21 17:22:09 +0200

    Add java-first

---
+++ apps/java-first/src/main/java/com/newrelic/javafirst/dto/ResponseDto.java
@@ -0,0 +1,15 @@
+package com.newrelic.javafirst.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String message;
+}
---
+++ apps/java-first/src/test/java/com/newrelic/javafirst/JavaFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newrelic.javafirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
