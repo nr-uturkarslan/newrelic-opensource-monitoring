e853dbc2f370e5bf787b99b9c127e143be17d29f
uturkarslan <ugurturkarslan@gmail.com>
2022-09-05 21:04:45 +0200

    Implement Terraform

---
+++ infra/terraform/01_platform/01_variables.tf
@@ -0,0 +1,61 @@
+### Variables ###
+
+## General
+
+# project
+variable "project" {
+  type    = string
+  default = "nr1"
+}
+
+# location_long
+variable "location_long" {
+  type    = string
+  default = "westeurope"
+}
+
+# location_short
+variable "location_short" {
+  type    = string
+  default = "euw"
+}
+
+# stage_long
+variable "stage_long" {
+  type    = string
+  default = "dev"
+}
+
+# stage_short
+variable "stage_short" {
+  type    = string
+  default = "d"
+}
+
+# instance
+variable "instance" {
+  type    = string
+  default = "001"
+}
+
+# platform
+variable "platform" {
+  type    = string
+  default = "platform"
+}
+
+## Resource Names
+
+# Resource Group
+variable "project_resource_group_name" {
+  type = string
+}
+
+# Kubernetes Cluster
+variable "project_kubernetes_cluster_name" {
+  type = string
+}
+
+variable "project_kubernetes_cluster_nodepool_name" {
+  type = string
+}
---
+++ infra/terraform/01_platform/05_kubernetes.tf
@@ -0,0 +1,49 @@
+### Kubernetes Cluster ###
+
+# Kubernetes Cluster
+resource "azurerm_kubernetes_cluster" "platform" {
+  name                = var.project_kubernetes_cluster_name
+  resource_group_name = azurerm_resource_group.platform.name
+  location            = azurerm_resource_group.platform.location
+
+  dns_prefix         = var.platform
+  kubernetes_version = "1.23.5"
+
+  node_resource_group = var.project_kubernetes_cluster_nodepool_name
+
+  default_node_pool {
+    name    = "system"
+    vm_size = "Standard_D2_v2"
+
+    node_labels = {
+      nodePoolName = "system"
+    }
+
+    enable_auto_scaling = true
+    node_count          = 1
+    min_count           = 1
+    max_count           = 2
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# Kubernetes Nodepool - General usage
+resource "azurerm_kubernetes_cluster_node_pool" "general" {
+  name                  = "general"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.platform.id
+  vm_size               = "Standard_DS2_v2"
+
+  orchestrator_version = "1.23.5"
+
+  node_labels = {
+    nodePoolName = "general"
+  }
+
+  enable_auto_scaling = true
+  node_count          = 2
+  min_count           = 2
+  max_count           = 3
+}
