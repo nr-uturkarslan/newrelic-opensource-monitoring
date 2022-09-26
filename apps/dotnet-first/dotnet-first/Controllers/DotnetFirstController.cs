9738e2469666d8dbe9fd7191f6059d5521523017
uturkarslan <ugurturkarslan@gmail.com>
2022-09-26 13:03:34 +0200

    Init

---
+++ apps/dotnet-first/dotnet-first/Controllers/DotnetFirstController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_first.Controllers;
+
+[ApiController]
+[Route("dotnet")]
+public class DotnetFirstController : ControllerBase
+{
+    private readonly ILogger<DotnetFirstController> _logger;
+
+    public DotnetFirstController(
+        ILogger<DotnetFirstController> logger
+    )
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "second")]
+    public ActionResult DotnetSecondMethod()
+    {
+        var result = new ObjectResult("Test");
+        //result.StatusCode = (int)response.StatusCode;
+        result.StatusCode = 200;
+        return result;
+    }
+}
+
