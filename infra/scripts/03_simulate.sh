2eec89991b364bf2934350cfc9c220d2df831570
uturkarslan <ugurturkarslan@gmail.com>
2022-09-30 16:43:06 +0200

    Update

--- infra/scripts/03_simulate.sh
+++ infra/scripts/03_simulate.sh
@@ -7,41 +7,70 @@ sudo kubectl run -it --rm simulator --image=debian:stable
 
 # After the test pod is running, you will gain access to the pod.
 # Then you can run the following commands:
-apt-get update -y
-apt-get install dnsutils -y
-apt-get install curl -y
-apt-get install vim -y
+apt-get update -y && \
+apt-get install dnsutils -y && \
+apt-get install curl -y && \
+apt-get install vim -y && \
 apt-get install jq -y
 
-# After the packages are installed, test the connectivity to the application pod:
-curl -Iv http://http://java-first.java.svc.cluster.local:8080/java/second
 #########
 
 #################################################
 ### Run the following script to generate load ###
 #################################################
+
 #!/bin/bash
 
-makeRestCall() {
+createValue() {
+
+  local endpoint=$1
 
   local randomValue=$(openssl rand -base64 12)
+  local randomTag=$(openssl rand -base64 12)
 
   echo -e "---\n"
 
-  curl -X POST "http://java-first.java.svc.cluster.local:8080/java/second" \
+  curl -X POST "http://${endpoint}" \
     -i \
     -H "Content-Type: application/json" \
     -d \
     '{
-        "message": "'"${randomValue}"'"
+        "value": "'"${randomValue}"'",
+        "tag": "'"${randomTag}"'"
     }'
 
   echo -e "\n"
-  sleep 2
+  sleep $REQUEST_INTERVAL
 }
 
-while true
-do
-  makeRestCall
-done
-#########
+####################
+### SCRIPT START ###
+####################
+
+# Set variables
+REQUEST_INTERVAL=2
+
+dotnetEndpoint="dotnet-first.dotnet.svc.cluster.local:8080/dotnet/second"
+javaEndpoint="java-first.java.svc.cluster.local:8080/java/second"
+
+createValue $dotnetEndpoint
+createValue $javaEndpoint
+
+nginxEndpoint="nginx-ingress-ingress-nginx-controller.nginx.svc.cluster.local:80"
+dotnetEndpoint="${nginxEndpoint}/dotnet/second"
+javaEndpoint="${nginxEndpoint}/java/second"
+
+createValue $dotnetEndpoint
+createValue $javaEndpoint
+
+# # Start making requests
+# while true
+# do
+
+#   # Create value
+#   for i in {1..3}
+#   do
+#     createValue $bravoEndpoint
+#     createValue $charlieEndpoint
+#   done
+# done
